4de18499b6b2b1ce23533699281d91e6fa510d03
SuicaLondon <suicadevinlondon@gmail.com>
2026-06-04 21:39:12 +0800

    Initial Commit

---
+++ Soniqo/SoniqoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoniqoApp.swift
+//  Soniqo
+//
+//  Created by suica on 04/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct SoniqoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
